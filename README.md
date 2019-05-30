f0579aa40b0b4ab57a02561784673981cb8c3357
talentless_guy <pilll.PL22@gmail.com>
2019-05-30 19:39:16 +0300

    trying to deploy multiroute...

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # komfy/api
 
+![Codacy](https://img.shields.io/codacy/grade/a4485f4982d54841930f0812b92f7c04.svg?style=flat-square)
+
 Komfy GraphQL API built with Go, GraphQL, PostgreSQL and GORM
 
 ## Local setup
--- index.go
+++ index.go
@@ -1,53 +1,53 @@
 package handler
 
 import (
-    // Go's packages
-    "fmt"
-    "net/http"
-    "encoding/json"
+	// Go's packages
+	"fmt"
+	"net/http"
+	"encoding/json"
 
-    // Community's packages 
-    "github.com/graphql-go/graphql"
+	// Community's packages 
+	"github.com/graphql-go/graphql"
 )
 
-func Handler(writer http.ResponseWriter, request *http.Request) {
-    // Creating a Schema
-    graphqlFields := graphql.Fields{
-        "hello": &graphql.Field{
-            Type: graphql.String,
-            Resolve: func (params graphql.ResolveParams) (interface{}, error) {
-                return "world", nil
-            },
-        },
-    }
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// Creating a Schema
+	graphqlFields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func (params graphql.ResolveParams) (interface{}, error) {
+					return "world", nil
+			},
+		},
+	}
 
-    newQuery := graphql.ObjectConfig{
-        Name: "MainQuery",
-        Fields: graphqlFields,
-    }
+	newQuery := graphql.ObjectConfig{
+		Name: "MainQuery",
+		Fields: graphqlFields,
+	}
 
-    schema, _err := graphql.NewSchema(graphql.SchemaConfig{
-        Query: graphql.NewObject(newQuery),
-    })
-    if _err != nil {
-        fmt.Fprintf(writer, "Unable to create the default schema: %v", _err)
-    }
+	schema, _err := graphql.NewSchema(graphql.SchemaConfig{
+		Query: graphql.NewObject(newQuery),
+	})
+	if _err != nil {
+		fmt.Fprintf(w, "Unable to create the default schema: %v", _err)
+	}
 
-    query := `{
-        hello
-    }`
+	query := `{
+			hello
+	}`
 
-    params := graphql.Params{
-        Schema: schema,
-        RequestString: query,
-    }
+	params := graphql.Params{
+		Schema: schema,
+		RequestString: query,
+	}
 
-    response := graphql.Do(params)
-    responseJSON, _err := json.Marshal(response)
-    if _err != nil {
-        fmt.Fprintf(writer, "Unable to marshal the graphql response")
-    }
+	response := graphql.Do(params)
+	responseJSON, _err := json.Marshal(response)
+	if _err != nil {
+		fmt.Fprintf(w, "Unable to marshal the graphql response")
+	}
 
-    fmt.Fprintf(writer, "%s", responseJSON)
+	fmt.Fprintf(w, "%s", responseJSON)
 
 }
