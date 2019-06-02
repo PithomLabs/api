f4c9da5193fb38da11f8146b538768dcb96426f0
Betelgeuse1 <avanalos@gmail.com>
2019-06-03 00:07:00 +0200

    Do some test with now routes

--- lambdas/randDict/randDict.go
+++ lambdas/randDict/randDict.go
@@ -3,17 +3,17 @@ package randDict
 import (
 	"fmt"
 	"net/http"
-	"io/ioutil"
-	"strings"
-	"time"
-	"math/rand"
+	//"io/ioutil"
+	//"strings"
+	//"time"
+	//"math/rand"
 )
 
 const (
 	maxPassLen = 20
 	
 )
-
+/*
 // Create a slice with the words from the words' file
 func createWordsSlice() ([]string, int, error) {
 	// Read all the words inside the file "wordsGist"
@@ -25,10 +25,11 @@ func createWordsSlice() ([]string, int, error) {
 	words := strings.Split(string(file), "\n")
 	return words, len(words), nil
 }
-
+*/
 // The /rand_dict handler function
 func Handler(writer http.ResponseWriter, req *http.Request) {
 	// Change the rand seed every time we asked for a password
+	/*
 	rand.Seed(time.Now().UnixNano())
 	words, wordsLen, err := createWordsSplit()
 	if err != nil {
@@ -42,4 +43,6 @@ func Handler(writer http.ResponseWriter, req *http.Request) {
 		pass = append(pass, word)
 	}
 	fmt.Fprintln(writer, strings.Join(pass, "-"))
+	*/
+	fmt.Fprintln(writer, "TEST")
 }
