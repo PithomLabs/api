a31eb68a724f8e03f34270c5a51f11cbb759e0e5
Betelgeuse1 <avanalos@gmail.com>
2019-06-03 14:37:34 +0200

    Make randDict endpoint work locally and with now.

--- dev-server/server.go
+++ dev-server/server.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/komfy/api/lambdas/rand"
+	index "github.com/komfy/api/lambdas"
+	"github.com/komfy/api/lambdas/randDict"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/rand", lambdas.rand.Handler)
+	http.HandleFunc("/", index.Handler)
+	http.HandleFunc("/rand", rand.Handler)
+	http.HandleFunc("/randDict", randDict.Handler)
 	http.ListenAndServe(":8080", nil)
 }
