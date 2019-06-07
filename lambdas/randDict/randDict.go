17e9baad6d1463cc2bb92ec05f39cfe66a06ad3a
Betelgeuse1 <avanalos@gmail.com>
2019-06-07 22:28:01 +0200

    Change randDict.go to use tha raw url of the wordsGist file instead of a local file

--- lambdas/randDict/randDict.go
+++ lambdas/randDict/randDict.go
@@ -2,6 +2,7 @@ package randDict
 
 import (
 	"fmt"
+	"log"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
@@ -9,22 +10,48 @@ import (
 	"time"
 )
 
-const maxPassLen int = 20
+const (
+	// Max password length (the password can actually be more than this number)
+	maxPassLen int = 20
+	// The raw github link for the dictionnary file
+	githubDictionary = "https://raw.githubusercontent.com/komfy/dict/master/wordsGist"
 
-// Create a slice with the words from the words' file
+)
+
+// Create a slice with the words from the github link
 func createWordsSlice() ([]string, int, error) {
 	// Read all the words inside the file "wordsGist"
 	file, err := ioutil.ReadFile("template/wordsGist")
+	// Make a get request to the github dictonary's link
+	resp, err := http.Get(githubDictionary)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 		return nil, 0, err
 	}
-	// Split the words into a slice
-	words := strings.Split(string(file), "\n")
-	return words, len(words), nil
+
+	// Read the github dictionary page body
+	body, bErr := ioutil.ReadAll(resp.Body)
+	if bErr != nil {
+		log.Fatal(bErr)
+		return nil, 0, bErr
+	}
+
+	// Defer the Body.Close() func and get any of the errors
+	// It could return, for more info read: 
+	// https://www.joeshaw.org/dont-defer-close-on-writable-files/
+	defer func() {
+		if bodyErr := resp.Body.Close(); err != nil {
+			log.Fatal(bodyErr)
+			err = bodyErr
+		}
+	}()
+
+	// Split the github dictionary's body into a slice
+	words := strings.Split(string(body), "\n")
+	return words, len(words), err
 }
 
-// The /rand_dict handler function
+// The /rand_dict handler functi
 func Handler(writer http.ResponseWriter, request *http.Request) {
 	// Change the rand seed every time we asked for a password
 	rand.Seed(time.Now().UnixNano())
