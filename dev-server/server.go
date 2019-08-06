5df2010cc2cfe258cbc85fa5df02706257473d90
Betelgeuse1 <avanalos@gmail.com>
2019-08-06 16:01:00 +0200

    Add .env file support in local development

--- dev-server/server.go
+++ dev-server/server.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
 
 	"github.com/komfy/api/lambdas"
 )
 
 func main() {
 	fmt.Println("Server is running on port 8080...")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", lambdas.IndexHandler)
 	http.HandleFunc("/rand", lambdas.RandHandler)
 	http.HandleFunc("/rand_dict", lambdas.RandDictHandler)
