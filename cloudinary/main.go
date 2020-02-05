c0efff28e7571a29a670dee5af60c34ed0c853b8
Ivan Malovanyi <maliovaniy.ivan@gmail.com>
2020-02-05 18:29:37 +0400

    finish to implement uploadFile func, need to add response handler and write tests

--- cloudinary/main.go
+++ cloudinary/main.go
@@ -2,10 +2,18 @@
 package cloudinary
 
 import (
+	"bytes"
+	"crypto/sha1"
 	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
 	"net/url"
-	"os"
+	"path/filepath"
+	"strconv"
+	"time"
 )
 
 //Service represents cloudinary service
@@ -64,20 +72,88 @@ func Dial(uri string) (*Service, error) {
 
 //UploadFile receives file, most probably from Multipart Form, uploads to cloud and returns
 //a link to the resource
-func (s *Service) UploadFile(file *os.File, randomPublicID bool) (string, error) {
+func (s *Service) UploadFile(fh *multipart.FileHeader, randomPublicID bool) (string, error) {
 	var publicID string
-	fi, err := file.Stat()
+	file, err := fh.Open()
 	if err != nil {
 		return "", err
 	}
-	if fi.Size() == 0 {
-		return "", fmt.Errorf("Not allowed to upload empty files: ", fi.Name())
+	fileBuf, err := ioutil.ReadAll(file)
+	defer file.Close()
+	if err != nil {
+		return "", err
 	}
+	if len(fileBuf) == 0 {
+		return "", fmt.Errorf("Not allowed to upload empty files: %s", fh.Filename)
+	}
+	filename := trimExt(fh.Filename)
+
+	//Creating a form body for request
+	buf := new(bytes.Buffer)
+	mw := multipart.NewWriter(buf)
+	//Writing a public_id field for request
 	if !randomPublicID {
-		publicID = fi.Name()
-	} else {
-		publicID = ""
+		publicID = filename
+		pi, err := mw.CreateFormField("public_id")
+		if err != nil {
+			return "", err
+		}
+		pi.Write([]byte(publicID))
+	}
+	//Writing an API key
+	ak, err := mw.CreateFormField("api_key")
+	if err != nil {
+		return "", err
+	}
+	ak.Write([]byte(s.apiKey))
+
+	//Writing timestamp
+	ts, err := mw.CreateFormField("timestamp")
+	if err != nil {
+		return "", err
+	}
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	ts.Write([]byte(timestamp))
+
+	//Writing signature
+	si, err := mw.CreateFormField("signature")
+	if err != nil {
+		return "", err
+	}
+	hash := sha1.New()
+	part := fmt.Sprintf("timestamp=%s%s", timestamp, s.apiSecret)
+	if !randomPublicID {
+		part = fmt.Sprintf("public_id=%s&%s", publicID, part)
+	}
+	io.WriteString(hash, part)
+	signature := fmt.Sprintf("%x", hash.Sum(nil))
+	si.Write([]byte(signature))
+
+	fi, err := mw.CreateFormFile("file", fh.Filename)
+	if err != nil {
+		return "", err
+	}
+	fi.Write(fileBuf)
+
+	err = mw.Close()
+	if err != nil {
+		return "", err
 	}
 
-	return "", nil
+	uploadURL := s.uploadURL.String()
+
+	req, err := http.NewRequest("POST", uploadURL, buf)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", mw.FormDataContentType())
+	resp, err := http.DefaultClient.Do(req)
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return string(body), nil
+}
+
+func trimExt(filename string) string {
+	fileExt := filepath.Ext(filename)
+	return filename[0 : len(filename)-len(fileExt)]
 }
