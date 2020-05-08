ec7e2e33bb3b56b71b4a1258eb5702bed6b1a998
Michael <mishaor@ukr.net>
2020-05-08 21:38:49 +0300

    server: fix CORS bug

--- server.go
+++ server.go
@@ -15,10 +15,6 @@ import (
 	"github.com/komfy/api/lambdas"
 )
 
-var (
-	frontendURL = os.Getenv("FRONTEND_URL")
-)
-
 const (
 	defaultPort string = "8080"
 )
@@ -111,7 +107,7 @@ func main() {
 
 // mainHandler (was known as AddCORSOnLocal) handles everything
 func mainHandler(resp http.ResponseWriter, req *http.Request) {
-	netutils.EnableCORS(&resp, frontendURL)
+	netutils.EnableCORS(&resp, os.Getenv("FRONTEND_URL"))
 	// We suppress the '/' at the beginning of the path
 	path := req.URL.Path[1:]
 
